0032480d27e3ae4e40156013cfdcb90c7f80950f
vvh0ann1 <vvh0ann1@gmail.com>
2024-11-18 16:36:15 +0900

    feat: 3 rds create

--- S3/main.tf
+++ S3/main.tf
@@ -32,7 +32,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3bucket_encrypti
 resource "aws_s3_bucket_public_access_block" "s3bucket_public-access" {
   bucket = aws_s3_bucket.project-team2-bucket.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
