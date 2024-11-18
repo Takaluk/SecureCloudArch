539654cba0e3c34588e66896c5494cddbf6ea110
julyjimongee <julysnowflake07@gmail.com>
2024-11-18 09:36:13 +0900

    feat: created efs(elastic file system)

---
+++ efs/efs.tf
@@ -0,0 +1,86 @@
+module "efs-sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "efs-sg"
+  description = "Security group for EFS(Elastic File System)"
+  vpc_id      = var.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      rule        = "ssh-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      rule        = "all-icmp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  use_name_prefix = false # 이름이 자동으로 변경되지 않도록
+}
+
+module "kms" {
+  source = "terraform-aws-modules/kms/aws"
+
+  key_usage   = "ENCRYPT_DECRYPT"
+
+  # Policy
+  key_administrators = [
+    "arn:aws:iam::761018879361:user/ewunng",
+    "arn:aws:iam::761018879361:user/takaluk",
+    "arn:aws:iam::761018879361:user/yeji",
+    "arn:aws:iam::761018879361:user/yunju"
+    ] # 키 관리자 권한
+  key_service_users  = [
+    "arn:aws:iam::761018879361:user/ewunng",
+    "arn:aws:iam::761018879361:user/takaluk",
+    "arn:aws:iam::761018879361:user/yeji",
+    "arn:aws:iam::761018879361:user/yunju"
+  ] # 키 사용자 권한
+}
+
+data "aws_subnet" "private_subnets" {
+  count = length(var.private_subnets_ids)
+
+  id = var.private_subnets_ids[count.index]
+}
+
+module "efs" {
+  tags = {
+    Name = "project-efs"
+  }
+
+  source = "terraform-aws-modules/efs/aws"
+
+  # File system
+  name           = "project_efs"
+  creation_token = "efs-token-for-project"
+  encrypted      = true
+  kms_key_arn    = module.kms.key_arn
+
+  # Mount targets / security group
+  mount_targets = {
+    "ap-northeast-2a" = {
+      subnet_id = var.public_subnets_ids[0]
+    }
+    "ap-northeast-2c" = {
+      subnet_id = var.public_subnets_ids[1]
+    }
+  }
+  security_group_description = "EFS security group"
+  security_group_vpc_id      = module.efs-sg.security_group_vpc_id
+
+  # 보안 그룹 규칙을 private subnet CIDR 블록으로 설정
+  security_group_rules = {
+    vpc = {
+      # relying on the defaults provided for EFS/NFS (2049/TCP + ingress)
+      description = "NFS ingress from VPC private subnets"
+      cidr_blocks = [for subnet in data.aws_subnet.private_subnets : subnet.cidr_block]
+    }
+  }
+}
---
+++ env-yeji/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "terraform-user"
+}
+
+module "yeji_vpc" {
+  source                           = "../vpc"
+  env_name                         = "yeji"
+  cidr                             = "10.2.0.0/16"
+  nat_primary_network_interface_id = module.yeji_ec2.nat_primary_network_interface_id
+}
+
+module "yeji_sg" {
+  source = "../sg"
+  vpc_id = module.yeji_vpc.vpc_id
+}
+
+module "yeji_ec2" {
+  source = "../ec2"
+
+  public_subnets_id  = module.yeji_vpc.public_subnets_id
+  private_subnets_id = module.yeji_vpc.private_subnets_id
+
+  bastion_host_sg_id = module.yeji_sg.bastion_host_sg_id
+  nat_sg_id          = module.yeji_sg.nat_sg_id
+  web_sg_id          = module.yeji_sg.web_sg_id
+  app_sg_id          = module.yeji_sg.app_sg_id
+}
+
+module "yeji_efs" {
+  source = "../efs"
+
+  vpc_id              = module.yeji_vpc.vpc_id
+  public_subnets_ids  = module.yeji_vpc.public_subnets_id
+  private_subnets_ids = module.yeji_vpc.private_subnets_id
+}
+
